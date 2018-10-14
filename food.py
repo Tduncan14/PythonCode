9628fa83de5a28dd73d9c40317c6240d0d8be6ab
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-10-14 08:39:28 -0500

    learn to keep moving forward

---
+++ food.py
@@ -0,0 +1,9 @@
+#Maitre D'
+# Demonstrates treating a value as a condition
+
+print("Welcome to the Chateau D' Food")
+print("It seems we are quite full this evening. \n")
+
+money = int(input("How many dollars do you slip the Maitre D'?"))
+
+if
