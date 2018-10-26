f7393918c7248eef6262ea9c55a578295de08149
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-10-25 22:43:21 -0500

    keep moving forward always

---
+++ websiteFolder/css/randomAccess.py
@@ -0,0 +1,13 @@
+#Random acess
+#Demonstration string indexing
+
+# imports the random module
+import random
+
+word ="index"
+
+print("The word is: ",word,"\n")
+
+#shows the length of thew word
+high=len(word)
+low = -len(word)
