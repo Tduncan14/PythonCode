df722cef1fc3273dd4e375c576b7c42037ebd7e0
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-10-31 20:57:10 -0500

    keep moving forward

---
+++ websiteFolder/css/hero.py
@@ -0,0 +1,12 @@
+#hero's inventory
+#Demonstrates tuple creation
+
+#create an empty tuple
+
+inventory = ()
+
+#treat the tuple as a condition
+if not inventory:
+    print("You are empty-handed")
+
+input("\nPress the enter key to continue")
