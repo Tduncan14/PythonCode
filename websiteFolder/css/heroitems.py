d5f3ee49680b85136ac92726f5dc5302a8167b80
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-10-31 21:16:59 -0500

    keep moving forward

---
+++ websiteFolder/css/heroitems.py
@@ -0,0 +1,16 @@
+#create a tuple with some items
+
+inventory =("sword","shield","armor","healing potion")
+
+# print the tuple
+
+print("\n The tuple inventory is: ")
+print(inventory)
+
+#print each element in the tuple
+
+print("\nYour items:")
+for item in inventory:
+    print (item)
+
+print("\n\nPress the enter key to exit.")
