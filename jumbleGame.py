398724bf5a6a9c3ed1efb55f73c86a933996a38c
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-11-01 21:44:09 -0500

    keep moving forward

--- jumbleGame.py
+++ jumbleGame.py
@@ -23,3 +23,45 @@ correct = word
 #create a jumble version of the world
 
 jumble =""
+
+while word:
+     position = random.randrange(len(word))
+     # creates a new version of jumble
+     jumble += word[position]
+     # creates a new version of word
+     word = word[:position] + word[(position + 1):]
+
+
+# start the game
+
+print("""
+
+  Welcome to Word Jumble
+
+  Unscramble the letters to make a word
+
+  """)
+
+print(" The jumble is : " , jumble)
+
+
+guess = input("\nYour guess: " )
+
+while guess != correct and guess != "" :
+  print("Sorry, that's not it.")
+  guess = input("Your guess: ")
+
+
+if guess == correct:
+    print = input("Your guess is correct. Pet yourself on the back")
+
+print("Thanks for playing")
+
+
+input("\n\nPress \ the enter key to exit")
+
+                  
+    
+
+     
+     
